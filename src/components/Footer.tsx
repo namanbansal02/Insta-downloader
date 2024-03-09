a8865385e2d484d663ff8a386fe1073395f3f44e
Naman Bansal <namanbansal2016@gmail.com>
2024-03-09 10:04:54 +0000

    final commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="mx-auto w-full max-w-screen-xl px-4 py-2 text-center text-secondary">
+      <div className="flex flex-col items-center gap-2 text-center text-xs">
+        <div
+          aria-label="Disclaimer"
+          className="text-sm font-semibold text-black"
+        >
+          We are not affiliated with Instagram or Meta
+        </div>
+        <div
+          aria-label="Disclaimer"
+          className="text-sm font-semibold text-black"
+        >
+          Made with 💌 <a href="https://github.com/namanbansal02" target="blank">@namanbansal02</a>
+        </div>
+        <div className="flex items-center gap-1">
+          <Link
+            href="/terms-of-service"
+            className="text-sm text-primary hover:underline"
+          >
+            Terms of Service
+          </Link>
+          <span aria-hidden="true">|</span>
+          <Link
+            href="/privacy-policy"
+            className="text-sm text-primary hover:underline"
+          >
+            Privacy Policy
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/configs/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Instagram Video Downloader",
+  description:
+    "Download Instagram videos quickly and easily with our free online Instagram video downloader. Save IG videos in high quality to your device effortlessly.",
+  url: "https://insta-downloader.vercel.app",
+  ogImageUrl: "https://insta-downloader.vercel.app/images/open-graph.png",
+  links: {
+    twitter: "https://twitter.com/namanbansal02",
+    github: "https://github.com/namanbansal02/Insta-downloader",
+  },
+};
